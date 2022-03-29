c3923045f08bf6553fe397d185a69a446f9d8776
ivopozzani <ivo.pozzani@gmail.com>
2022-03-29 15:55:19 -0300

    feat: Added Exercises controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :routines
-  devise_for :users
   root 'routines#index'
+  devise_for :users
+  resources :exercises
+  resources :routines
 end
--- spec/requests/routines_spec.rb
+++ spec/requests/routines_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Routines', type: :request do
+<<<<<<< HEAD
   let(:user) { create(:user) }
   let(:headers) { { Authorization: sign_in(user) } }
   let(:routine) { create(:routine) }
@@ -10,21 +11,37 @@ RSpec.describe 'Routines', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
       get routines_path
+=======
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/routines/index'
+>>>>>>> 5f07509... feat: added Exercises controller and views
       expect(response).to have_http_status(:success)
     end
 
+<<<<<<< HEAD
     it 'assigns @routines' do
       routines = []
       3.times { routines << create(:routine) }
 
       get routines_path
       expect(assigns(:routines)).to eq(routines)
+=======
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/routines/create'
+      expect(response).to have_http_status(:success)
+>>>>>>> 5f07509... feat: added Exercises controller and views
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
+<<<<<<< HEAD
       get routine_path(routine.id), headers: headers
+=======
+      get '/routines/show'
+>>>>>>> 5f07509... feat: added Exercises controller and views
       expect(response).to have_http_status(:success)
     end
 
@@ -36,11 +53,16 @@ RSpec.describe 'Routines', type: :request do
 
   describe 'GET /new' do
     it 'returns http success' do
+<<<<<<< HEAD
       get new_routine_path, headers: headers
+=======
+      get '/routines/new'
+>>>>>>> 5f07509... feat: added Exercises controller and views
       expect(response).to have_http_status(:success)
     end
   end
 
+<<<<<<< HEAD
   describe 'POST /create' do
     subject { post routines_path, headers:, params: { routine: { name: 'Routine' } } }
 
@@ -57,10 +79,23 @@ RSpec.describe 'Routines', type: :request do
   describe 'GET /edit' do
     it 'returns http success' do
       get edit_routine_path(routine.id), headers: headers
+=======
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get '/routines/edit'
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'GET /update' do
+    it 'returns http success' do
+      get '/routines/update'
+>>>>>>> 5f07509... feat: added Exercises controller and views
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+<<<<<<< HEAD
   describe 'PATCH /update' do
     it 'returns http found' do
       patch routine_path(routine.id), headers: headers, params: { routine: { name: 'lorem' } }
@@ -88,4 +123,12 @@ RSpec.describe 'Routines', type: :request do
       end.to raise_error(ActiveRecord::RecordNotFound, "Couldn't find Routine with 'id'=1")
     end
   end
+=======
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/routines/destroy'
+      expect(response).to have_http_status(:success)
+    end
+  end
+>>>>>>> 5f07509... feat: added Exercises controller and views
 end
