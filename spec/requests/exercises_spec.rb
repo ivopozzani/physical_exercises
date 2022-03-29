5f0750975e10a298eef91127d2bf6dca6ecd68c2
ivopozzani <ivo.pozzani@gmail.com>
2022-03-29 15:55:19 -0300

    feat: added Exercises controller and views

--- spec/requests/exercises_spec.rb
+++ spec/requests/exercises_spec.rb
@@ -2,52 +2,52 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Routines', type: :request do
+RSpec.describe 'Exercises', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/routines/index'
+      get '/exercises/index'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /create' do
     it 'returns http success' do
-      get '/routines/create'
+      get '/exercises/create'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/routines/show'
+      get '/exercises/show'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /new' do
     it 'returns http success' do
-      get '/routines/new'
+      get '/exercises/new'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /edit' do
     it 'returns http success' do
-      get '/routines/edit'
+      get '/exercises/edit'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /update' do
     it 'returns http success' do
-      get '/routines/update'
+      get '/exercises/update'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /destroy' do
     it 'returns http success' do
-      get '/routines/destroy'
+      get '/exercises/destroy'
       expect(response).to have_http_status(:success)
     end
   end
