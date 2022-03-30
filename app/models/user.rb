a395f2f322382cc1ad1099b369e01f5b51927964
ivopozzani <ivo.pozzani@gmail.com>
2022-03-30 15:31:51 -0300

    feat: added password complexity

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    if password.present? && !password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)./)
+      errors.add :password, 'must include at least one lowercase letter, one uppercase letter, and one digit'
+    end
+  end
 end
