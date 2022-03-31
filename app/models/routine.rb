d53657185112f5313a984b44387a3eb4cef0ea6c
ivopozzani <ivo.pozzani@gmail.com>
2022-03-31 13:45:45 -0300

    feat: remove routine name validations

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,6 +2,4 @@
 
 class Routine < ApplicationRecord
   has_and_belongs_to_many :exercises
-
-  validates :name, presence: true, uniqueness: true
 end
--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -5,11 +5,6 @@ require 'rails_helper'
 RSpec.describe Routine, type: :model do
   subject { create(:routine) }
 
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
-  end
-
   describe 'associations' do
     it { is_expected.to have_and_belong_to_many(:exercises) }
   end
